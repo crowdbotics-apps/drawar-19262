71081e6e69fde804e4321de00832a2667cb33429
Crowdbotics <team@crowdbotics.com>
2020-07-30 14:01:33 +0000

    Initial commit.

---
+++ backend/drawar_19262/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drawar_19262 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drawar_19262.settings')
+
+application = get_wsgi_application()
